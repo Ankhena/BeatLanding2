dd1939347ace67f2f393392d25818fd98c9c7e72
Ankhena <Ankhena@users.noreply.github.com>
2022-07-19 20:37:55 +0300

    Новая страница О проекте, блок Почему стоит попробовать?

---
+++ src/js/_highchart-inits.js
@@ -0,0 +1,296 @@
+///////////////////
+// highchart-inits
+
+
+if (document.querySelector('#try-why-graph-1')) {
+  Highcharts.chart('try-why-graph-1', {
+    chart: {
+      type: 'spline',
+      style: { "fontFamily": "" },
+    },
+    title: {
+      text: ''
+    },
+    // xAxis: {
+    //   categories: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    //   labels: {
+    //     style: {
+    //       fontSize: "16px",
+    //       color: "#373943"
+    //     }
+    //   }
+    // },
+    yAxis: {
+      title: {
+        text: ''
+      },
+      labels: {
+        formatter: function () {
+          return (this.value / 1000) + 'к';
+        },
+        style: {
+          fontSize: "16px",
+          color: "#373943"
+        }
+      },
+    },
+    tooltip: {
+      crosshairs: true,
+      shared: true,
+      useHTML: true
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          radius: 4,
+          lineColor: 'transparent',
+          lineWidth: 1
+        }
+      }
+    },
+    series: [
+
+      {
+        name: 'Портфель СЕО',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [1500, 2100, 3500, 5500, 4200, 6500, 7200],
+        color: "#3E54D8"
+      },
+      {
+        name: 'S&P 500',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [2200, 2300, 5200, 3700, 4000, 7800, 3800],
+        color: "#DE4355"
+      },
+
+    ]
+  });
+
+}
+
+/////////////////////////////
+
+if (document.querySelector('#try-why-graph-2')) {
+  Highcharts.chart('try-why-graph-2', {
+    chart: {
+      type: 'spline',
+      style: { "fontFamily": "" },
+    },
+    title: {
+      text: ''
+    },
+    // xAxis: {
+    //   categories: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    //   labels: {
+    //     style: {
+    //       fontSize: "16px",
+    //       color: "#373943"
+    //     }
+    //   }
+    // },
+    yAxis: {
+      title: {
+        text: ''
+      },
+      labels: {
+        formatter: function () {
+          return (this.value / 1000) + 'к';
+        },
+        style: {
+          fontSize: "16px",
+          color: "#373943"
+        }
+      },
+    },
+    tooltip: {
+      crosshairs: true,
+      shared: true,
+      useHTML: true
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          radius: 4,
+          lineColor: 'transparent',
+          lineWidth: 1
+        }
+      }
+    },
+    series: [
+
+      {
+        name: 'Портфель СЕО',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [1500, 2100, 3500, 5500, 4200, 6500, 7200],
+        color: "#3E54D8"
+      },
+      {
+        name: 'S&P 500',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [2200, 2300, 5200, 3700, 4000, 7800, 3800],
+        color: "#DE4355"
+      },
+
+    ]
+  });
+
+}
+
+/////////////////////////
+
+if (document.querySelector('#try-why-graph-3')) {
+  Highcharts.chart('try-why-graph-3', {
+    chart: {
+      type: 'spline',
+      style: { "fontFamily": "" },
+    },
+    title: {
+      text: ''
+    },
+    // xAxis: {
+    //   categories: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    //   labels: {
+    //     style: {
+    //       fontSize: "16px",
+    //       color: "#373943"
+    //     }
+    //   }
+    // },
+    yAxis: {
+      title: {
+        text: ''
+      },
+      labels: {
+        formatter: function () {
+          return (this.value / 1000) + 'к';
+        },
+        style: {
+          fontSize: "16px",
+          color: "#373943"
+        }
+      },
+    },
+    tooltip: {
+      crosshairs: true,
+      shared: true,
+      useHTML: true
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          radius: 4,
+          lineColor: 'transparent',
+          lineWidth: 1
+        }
+      }
+    },
+    series: [
+
+      {
+        name: 'Портфель СЕО',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [1500, 2100, 3500, 5500, 4200, 6500, 7200],
+        color: "#3E54D8"
+      },
+      {
+        name: 'S&P 500',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [2200, 2300, 5200, 3700, 4000, 7800, 3800],
+        color: "#DE4355"
+      },
+
+    ]
+  });
+
+}
+
+//////////////////////////
+
+if (document.querySelector('#try-why-graph-4')) {
+  Highcharts.chart('try-why-graph-4', {
+    chart: {
+      type: 'spline',
+      style: { "fontFamily": "" },
+    },
+    title: {
+      text: ''
+    },
+    // xAxis: {
+    //   categories: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+    //   labels: {
+    //     style: {
+    //       fontSize: "16px",
+    //       color: "#373943"
+    //     }
+    //   }
+    // },
+    yAxis: {
+      title: {
+        text: ''
+      },
+      labels: {
+        formatter: function () {
+          return (this.value / 1000) + 'к';
+        },
+        style: {
+          fontSize: "16px",
+          color: "#373943"
+        }
+      },
+    },
+    tooltip: {
+      crosshairs: true,
+      shared: true,
+      useHTML: true
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          radius: 4,
+          lineColor: 'transparent',
+          lineWidth: 1
+        }
+      }
+    },
+    series: [
+
+      {
+        name: 'Портфель СЕО',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [1500, 2100, 3500, 5500, 4200, 6500, 7200],
+        color: "#3E54D8"
+      },
+      {
+        name: 'S&P 500',
+        marker: {
+          symbol: 'circle'
+        },
+        data: [2200, 2300, 5200, 3700, 4000, 7800, 3800],
+        color: "#DE4355"
+      },
+
+    ]
+  });
+
+}
+
+
+
+
+
+// end highchart-inits
+////////////////////
--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,17 @@ $(document).ready(function() {
     --include("_burger.js")
     --include("_modal.js");
 
+    --include("scripts/highcharts.min.js");
+    --include("_highchart-inits.js");
+
     $('.faq-container-items-item').click(function () {
         let header = $(this).children();
         let content = header.next();
         header.toggleClass("opened");
-    
+
         if (!content.is(':visible')) {
             content.slideDown('normal');
-        } 
+        }
         else {
             content.slideUp('normal');
         }
@@ -35,20 +38,20 @@ $(document).ready(function() {
 
     function initSubscribeChanger() {
         let toggler = document.querySelector(".subscribe-changer input[type='checkbox']");
-    
+
         if (toggler !== null) {
             let costsBlock = document.querySelectorAll(".costSwitcher");
             let togglerDescr = document.querySelectorAll(".checkbox-toggle__descr");
-        
+
             let subscribeCosts = [["290 руб.", "690 руб.", "890 руб."], ["1290 руб.", "2690 руб.", "3290 руб."]];
             let subscribeNames = ["/ месяц", "/ год"];
-        
-        
+
+
             toggler.addEventListener("change", function() {
                 togglerDescr.forEach((item, i) => {
                     item.classList.toggle("checkbox-toggle__descr--light");
                 });
-        
+
                 costsBlock.forEach((item, i) => {
                     item.querySelector("p strong").innerHTML = subscribeCosts[+toggler.checked][i];
                     item.querySelector("p span").innerHTML = subscribeNames[+toggler.checked];
@@ -57,7 +60,9 @@ $(document).ready(function() {
             });
         }
     }
-    
+
     initSubscribeChanger();
-    
+
 });
+
+
